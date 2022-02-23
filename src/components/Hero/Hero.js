3e18e3ecfa446e9bba487bd62210c1bace8da915
LyzzHans <lyzz.hans@gmail.com>
2022-02-23 10:50:57 -0500

    edit package.json

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -11,7 +11,7 @@ const Hero = (props) => (
         Hi! I'm Lyzz! 
       </SectionTitle>
       <SectionText>
-      I'm a coding hippie. Two things fascinate me: people and coding. I am constantly curious about our interconnectivity and a tireless advocate for inclusiveness and diversity. During the pandemic, I enrolled in a Software Development Bootcamp and learned that I am obsessed with learning all things code and that test driven development is my jam. 
+      I'm a coding hippie. Two things fascinate me: people and coding. I am constantly curious about our inter-connectivity and a tireless advocate for inclusiveness and diversity. During the pandemic, I enrolled in a Software Development Bootcamp and learned that I am obsessed with learning all things code and that test driven development is my jam. 
 
 My enthusiasm for people and coding transpires well in Agile environments. I am always willing to help a fellow developer with questions and advice and I love sparking joy in teams. 
 
